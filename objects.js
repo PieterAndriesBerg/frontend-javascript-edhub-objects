ed1aceea17e225ac26d035d0dcfb5e3424cf5a3a
Pieter-Andries van der Berg <p-a.berg@outlook.com>
2020-10-20 12:11:10 +0200

    Made assignments.

--- objects.js
+++ objects.js
@@ -10,17 +10,19 @@ const product = {
     "b-h-d": "17 x 24,6 x 22,3 cm",
 };
 
-// 1a. Log de naam van de serie in de console.
+// 1a. Log de naam van dek serie in de console.
+console.log(product.series)
 
 
 // 1b. Verander de kleur van het product in "Roze" en log het object in de console
-
+product.color = "Roze";
+console.log(product.color);
 
 // 1c. Log de tweede materiaalsoort ("kunststof") in de console
-
+console.log(product.material[1]);
 
 // 1d. Log de afmetingen (b-h-d) in de console
-
+console.log(product["b-h-d"]);
 
 
 /* Opdracht 2 */
@@ -28,14 +30,23 @@ const product = {
 // en haar materialen om een string terug te geven die het product beschrijft
 // ---- Verwachte uitkomst:
 // "De Smeg Waterkoker is gemaakt van roestvrijstaal en kunststof"
-
-
+const slogan = (product) => {
+    return "De " + product.productName + " is gemaakt van " + product.material[0] + " en " + product.material[1];
+}
+console.log(slogan(product))
 
 /* Opdracht 3 */
 // Maak een object met daarin jouw voornaam (string), achternaam (string),
 // leeftijd (getal) en geboortedatum (date object). De datum hoeft niet geformat te worden.
 // Log dit in de console.
+const personInfo = {
+    firstName: "Pieter-Andries",
+    lastName: "van der Berg",
+    age: 26,
+    birthDate: new Date(1994, 7, 26)
+};
 
+console.log(personInfo);
 
 
 /* Opdracht */
@@ -44,3 +55,7 @@ const product = {
 // ---- Verwachte uitkomsten:
 // { age: 27 } geeft "Jij bent 27 jaar oud."
 // { age: 56 } geeft "Jij bent 56 jaar oud."
+const getAge = (personInfo) => {
+    return "Jij bent " + personInfo.age + " jaar oud.";
+}
+console.log(getAge(personInfo));
